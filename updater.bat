73a2970e709a090cc2978a902552b379383df9ef
LIL-JRG <00740249@red.unid.mx>
2024-10-15 13:59:24 -0600

    upt

--- updater.bat
+++ updater.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
 SET $Echo=FOR %%I IN (1 2) DO IF %%I==2 (SETLOCAL EnableDelayedExpansion ^& FOR %%A IN (^^^!Text:""^^^^^=^^^^^"^^^!) DO ENDLOCAL ^& ENDLOCAL ^& ECHO %%~A) ELSE SETLOCAL DisableDelayedExpansion ^& SET Text=
 
 SETLOCAL DisableDelayedExpansion
@@ -6,13 +8,10 @@ echo.
 echo.
 echo Downloading Latest Update . . .
 powershell (New-Object System.Net.WebClient).Downloadfile('https://github.com/LIL-JRG/cjrtoolkit/archive/refs/heads/main.zip', 'cjrtoolkit-latest.zip') 
-
 echo Extracting Files
-powershell.exe Expand-Archive -Path cjrtoolkit-latest.zip -Force
-
+powershell.exe Expand-Archive -Path cjrtoolkit-latest.zip -Force 
 echo Replacing Files
 xcopy /s "cjrtoolkit-latest/cjrtoolkit-main" "*" /Y
-
 echo Cleaning Up Temp Files !
 powershell Remove-Item -Path cjrtoolkit-latest.zip -Force
 powershell Remove-Item -Path cjrtoolkit-latest -Force -Recurse
