b2b528fcaa17d67e2090ad75dde62d7de221582d
LIL-JRG <00740249@red.unid.mx>
2024-11-05 16:52:27 -0600

    newrq

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@
 
 echo Installing requirements, please wait
 pip install -r requirements.txt > nul
+python -m spacy download es_core_news_sm > nul
 echo Done. Open start.bat to run the program
 
 pause
